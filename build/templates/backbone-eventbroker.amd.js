cb3cffca27edade59bf3618fed9c570d6a2304c3
Sven Lito <me@svenlito.com>
2012-05-18 11:25:08 +0100

    refining build process so AMD and browser/node.js modules can be built

---
+++ build/templates/backbone-eventbroker.amd.js
@@ -0,0 +1,28 @@
+/**
+ * Backbone Eventbroker v{{version}}
+ *
+ * NOTE:
+ * This version is for use with RequireJS
+ * If using regular <script> tags to include your files, use backbone-memory.min.js
+ *
+ * Copyright (c) 2012 Eric Feminella, Sven Lito
+ *
+ * License and more information at:
+ * http://code.ericfeminella.com/license/LICENSE.txt
+ */
+define([
+  'jquery',
+  'backbone',
+  'underscore'
+],
+
+function($, Backbone, _) {
+
+  "use strict";
+
+  {{body}}
+
+  //Exports
+  return Backbone.EventBroker;
+
+});
